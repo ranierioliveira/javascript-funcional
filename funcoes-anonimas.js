712f15c44fc8fdea081bb81561db50e12bb282a3
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-03-28 16:40:09 -0300

    Spread e IIFE em JavaScript

--- funcoes-anonimas.js
+++ funcoes-anonimas.js
@@ -1,3 +1,7 @@
 (function (x, y){
     console.log(x + y);
 })(25, 15);
+
+//IIFE
+const nome = "Ranierí";
+(() => {console.log("Olá, mundo!")})();
---
+++ spread.js
@@ -0,0 +1,10 @@
+const soma = (...numeros) => {
+    console.log(numeros);
+    let aux = 0;
+    for (c of numeros){
+        aux += c;
+    }
+    return aux;
+}
+
+console.log(soma(2, 5, 10, 2));
