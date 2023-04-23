5fc9fb636d9f0983e06824d9afacd0a0ddd780e8
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-04-23 20:36:41 -0300

    Revisão de for in e for of

---
+++ revisao4.js
@@ -0,0 +1,11 @@
+const numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+for (i of numeros){
+    console.log(i);
+}
+
+const notas = {n1: 4, n2: 8, n3: 9, n4: 10};
+
+for (i in notas){
+    console.log(notas[i]);
+}
